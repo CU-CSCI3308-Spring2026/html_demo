58bdbba51cdcda2d75d89edc42665594e095b49d
SreeshaNath <31390313+SreeshaNath@users.noreply.github.com>
2026-02-11 15:15:49 -0700

    Update query1.sql

--- init_db/queries/query1.sql
+++ init_db/queries/query1.sql
@@ -1,5 +1,10 @@
--- write query to get details of customers 
+-- write query to get details of customers (SELECT)
 
 
--- Let's sort these alphabetically
+-- Let's sort these alphabetically by name
+
+
+-- Now let's get the details of the first customer in the sorted table
+
+
  
